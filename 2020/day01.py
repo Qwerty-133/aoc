f6f2add1c6edceec36b2e74971692cdc615c6c47
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2020-12-01 11:34:29 +0530

    2020 day 1 one-liner

--- 2020/day01.py
+++ 2020/day01.py
@@ -1,5 +1,6 @@
 import itertools
-
+import functools
+import operator
 from pathlib import Path
 
 input_file_name = Path(__file__).with_suffix('.txt').name
@@ -7,21 +8,14 @@ with open(Path('2020/inputs') / input_file_name) as f:
     data = list(map(int, f.read().split()))
 
 
-def func(nums):
-    for num, num2 in itertools.combinations(nums, 2):
-        if num + num2 == 2020:
-            return num * num2
+def sum_to_2020(nums, count):
+    return next(functools.reduce(operator.mul, nums)
+                for nums in itertools.combinations(nums, count)
+                if sum(nums) == 2020)
 
 
-print(func(data))
+print(sum_to_2020(data, 2))
 
 # Part Two
 
-
-def three(nums):
-    for num, num2, num3 in itertools.combinations(nums, 3):
-        if num + num2 + num3 == 2020:
-            return num * num2 * num3
-
-
-print(three(data))
+print(sum_to_2020(data, 3))
